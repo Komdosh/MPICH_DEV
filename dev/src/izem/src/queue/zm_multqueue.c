a6bd64505f881b61db9c7d254a63b8de2823e34b
Andrey Tabakov <Komdosh@yandex.ru>
2019-11-06 13:46:48 +0300

    Update zm_multqueue.c
    remove inline functions

--- dev/src/izem/src/queue/zm_multqueue.c
+++ dev/src/izem/src/queue/zm_multqueue.c
@@ -26,16 +26,11 @@ int zm_multqueue_init(zm_multqueue_t *q) {
 
 uint64_t gettid() {
     pthread_t ptid = pthread_self();
-    uint64_t threadId = 0;
-    memcpy(&threadId, &ptid, std::min(sizeof(threadId), sizeof(ptid)));
+    uint64_t threadId = (uint64_t) ptid;
     return threadId;
 }
 
-inline int get_random_queue_index_half(int threads_num) { return rand() % (threads_num / 2); }
-
-inline int get_random_queue_index(int threads_num) { return rand() % threads_num; }
-
-inline void insert(zm_multqueue_t *q, void *data) {
+int zm_multqueue_enqueue(zm_multqueue_t *q, void *data) {
     int queue_index;
 
     uint64_t tid = gettid();
@@ -43,23 +38,18 @@ inline void insert(zm_multqueue_t *q, void *data) {
     pthread_mutex_t *locks = q->locks;
     do {
         if (tid < q->threads_num) {
-            queue_index = get_random_queue_index_half(q->threads_num);
+            queue_index = (rand() % (q->threads_num / 2));
         } else {
-            queue_index = get_random_queue_index_half(q->threads_num) + q->threads_num / 2;
+            queue_index =  return (rand() % (q->threads_num / 2)) + (q->threads_num / 2);
         }
     } while (pthread_mutex_lock(&locks[queue_index]) != 0);
     //printf("multqueue %d enqueue\n", queue_index);
     zm_glqueue_enqueue(&q->queues[queue_index], data);
     pthread_mutex_unlock(&locks[queue_index]);
-}
-
-int zm_multqueue_enqueue(zm_multqueue_t *q, void *data) {
-    insert(q, data);
     return 0;
 }
 
-
-inline void pop(zm_multqueue_t *q, void **data) {
+int zm_multqueue_dequeue(zm_multqueue_t *q, void **data) {
     int queue_index;
     pthread_mutex_t *locks = q->locks;
     int it = 0;
@@ -72,15 +62,11 @@ inline void pop(zm_multqueue_t *q, void **data) {
             queue_index = tid * QUEUES_PER_CORE + 1;
             ++it;
         } else {
-            queue_index = get_random_queue_index_half(q->threads_num);
+            queue_index = (rand() % (q->threads_num / 2));
         }
     } while (pthread_mutex_lock(&locks[queue_index]) != 0);
     //printf("multqueue %d dequeue\n", queue_index);
     zm_glqueue_dequeue(&q->queues[queue_index], data);
     pthread_mutex_unlock(&locks[queue_index]);
-}
-
-int zm_multqueue_dequeue(zm_multqueue_t *q, void **data) {
-    pop(q, data);
     return 1;
 }
